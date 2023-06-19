f1cc72fbeb2c8de962d72ad367dbbe99dc76077c
ardit <sanjayvenkat1234@gmail.com>
2023-06-19 12:05:38 +0530

    mistake

--- web.py
+++ web.py
@@ -15,7 +15,7 @@ st.title("My Todo App")
 
 
 for index,todo in enumerate(todos):
-    checkbox = st.checkbox(todo,key="todo")
+    checkbox = st.checkbox(todo,key=todo)
     if checkbox:
         todos.pop(index)
         functions.write_todos(todos)
@@ -25,6 +25,3 @@ for index,todo in enumerate(todos):
 
 st.text_input(label="",placeholder="Add a new todo....",
               on_change=add_todo,key='new_todo')
-print("Hello")
-
-st.session_state
